d221d1cf36b1f69696c9f62bc77e45415944a762
Carlos Camargo <carlosa.camargo16@gmail.com>
2018-06-11 15:12:56 -0500

    final

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php header( 'location: /testapplogin.html' ); ?>
+<?php header( 'location: /testappcategories.html' ); ?>
+<?php header( 'location: /testappservicios' ); ?>
+<?php header( 'location: /testappservicios2' ); ?>
+<?php header( 'location: /testappmapala' ); ?>
+<?php header( 'location: /testappmapala2' ); ?>
+<?php header( 'location: /testappmapala3' ); ?>
+<?php header( 'location: /testappmaproma' ); ?>
+<?php header( 'location: /testappmapdoc' ); ?>
+<?php header( 'location: /testappmapapor' ); ?>
+<?php header( 'location: /testappmapacond' ); ?>
+<?php header( 'location: /testappmapaza' ); ?>
+
+
