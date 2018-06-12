8b876bddeca1ab03478fadea28ff8a2602384103
Carlos Camargo <carlosa.camargo16@gmail.com>
2018-06-11 19:19:06 -0500

    Hope commit

--- testapp.js
+++ testapp.js
@@ -1,55 +1,19 @@
-confirmados = [
-    {
-      "id":"0001",
-      "city":"CDMX",
-      "customer":"Román López",
-      "addres_street": "Zamora 177, Int 101",
-      "addres_neighborhood": "Condesa",
-      "service_type": "Recurrente",
-      "date": "30 feb",
-      "hour": "7:00 AM",
-      "lenght": "4",
-      "lat_service": "19.412901",
-      "long_service": "-99.180274"
-    },
-    {
-      "id":"0002",
-      "city":"CDMX",
-      "customer":"Mauricio Morales",
-      "addres_street": "Amatlán 18",
-      "addres_neighborhood": "Condesa",
-      "service_type": "Expres",
-      "date": "30 feb",
-      "hour": "12:00 AM",
-      "lenght": "3",
-      "lat_service": "19.413958",
-      "long_service": "-99.174357"
-    },
-    {
-      "id":"0003",
-      "city":"CDMX",
-      "customer":"Roberto Salgado",
-      "addres_street": "Atlixco 5",
-      "addres_neighborhood": "Condesa",
-      "service_type": "Recurrente",
-      "date": "30 feb",
-      "hour": "4:00 PM",
-      "lenght": "3",
-      "lat_service": "19.415771",
-      "long_service": "-99.172825"
-    }
-
-];
 
 function servicios() {
+  var direccion = "";
+  var fecha = "";
   var content = "";
 
-  for(var i = 0; i < confirmados.length; i++){
-    content += '<div class="servicio">' + confirmados[i].addres_neighborhood + '</div>'
-    content += '<div class="servicio">' + confirmados[i].date + '</div>'
-    content += '<div class="servicio">' + confirmados[i].hour + '</div>'
-    content += '<div class="servicio">' + confirmados[i].lenght + '</div>'
-    content += '<div class="servicio">' + confirmados[i].service_type + '</div>'
+  for(var i = 0; i < my_services.length; i++){
+   
+    direccion += '<id="direccion">' + my_services[i].addres_neighborhood + '</div>'
+    document.getElementById("direccion").innerHTML = direccion;
+    console.log(direccion)
+    content += '<div class="servicio">' + my_services[i].date + '</div>'
+    document.getElementById("confirmados").innerHTML = fecha;
+    content += '<div class="servicio">' + my_services[i].hour + '</div>'
+    content += '<div class="servicio">' + my_services[i].lenght + '</div>'
+    content += '<div class="servicio">' + my_services[i].service_type + '</div>'
 
   }
 
