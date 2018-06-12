30c748360789280d8b753752318c92d90459fc25
Carlos Camargo <carlosa.camargo16@gmail.com>
2018-06-11 21:11:09 -0500

    Test

--- testapp.js
+++ testapp.js
@@ -1,21 +1,24 @@
 
 function servicios() {
   var direccion = "";
-  var fecha = "";
+  var date1 = "";
   var content = "";
 
   for(var i = 0; i < my_services.length; i++){
-   
-    direccion += '<id="direccion">' + my_services[i].addres_neighborhood + '</div>'
+    
+    direccion += '<id="direccion">' + my_services[i].addres_street + '</div>'
+  }
     document.getElementById("direccion").innerHTML = direccion;
     console.log(direccion)
-    content += '<div class="servicio">' + my_services[i].date + '</div>'
-    document.getElementById("confirmados").innerHTML = fecha;
+  
+    
+    content += '<div id="date1">' + my_services[i].date + '</div>'
+    document.getElementById("date1").innerHTML = date1;
+    
     content += '<div class="servicio">' + my_services[i].hour + '</div>'
     content += '<div class="servicio">' + my_services[i].lenght + '</div>'
     content += '<div class="servicio">' + my_services[i].service_type + '</div>'
 
   }
 
-  document.getElementById("confirmados").innerHTML = content;
-}
+  document.getElementById("servicios").innerHTML = content;
